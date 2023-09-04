172425836f80c7a7271f1fe482ec64cb7b7a3c89
ewen-r <ewen.reynolds@gmail.com>
2023-09-04 15:05:07 +0100

    Added dummy data for quiz

--- routes/test_data.js
+++ routes/test_data.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /*
   © Copyright 2023-2023 E Reynolds, Inc. All rights reserved.
 
@@ -9,7 +10,7 @@
 /* spellchecker: disable */
 
 /** Dummy data. */
-const testData = [
+export const quotesTestData = [
   {
     "id": 1,
     "quote": "Life isn’t about getting and having, it’s about giving and being.",
@@ -162,4 +163,308 @@ const testData = [
   }
 ];
 
-export default testData;
+
+
+/** Dev use only
+  * @ignore */
+const originator1 = {
+  username: 'Ewen Reynolds',
+  createDate: '22-Aug-2023',
+  modifyDate: ''
+};
+
+/** Dev use only
+  * @ignore */
+const originator2 = {
+  username: 'Kay Reynolds',
+  createDate: '2-Jun-2020',
+  modifyDate: '13-Aug-2021'
+};
+
+/** Dev use only
+  * @ignore */
+const questionTF01 = {
+  uuid: 'questionTF01',
+  originatorInfo: originator1,
+  topic: 'sport',
+  questionType: 'True or False',
+  difficulty: 'Easy',
+  statement: 'True or false.. Nick Faldo is a famous golfer?',
+  multiChoiceOptions: [
+    'true', 'false'
+  ],
+  answer: {
+    match: 'true',
+    statement: 'He is one of England\'s most successful golfers.'
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionTF02 = {
+  uuid: 'questionTF02',
+  originatorInfo: originator2,
+  topic: 'general knowledge',
+  questionType: 'True or False',
+  difficulty: 'Easy',
+  statement: 'True or false.. The sky is purple with green spots?',
+  multiChoiceOptions: [
+    'true', 'false'
+  ],
+  answer: {
+    match: 'false',
+    statement: 'Of course it isn\'t... everyone knows it\'s purple with YELLOW spots 😆'
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionTF03 = {
+  uuid: 'questionTF03',
+  originatorInfo: originator2,
+  topic: 'general knowledge',
+  questionType: 'True or False',
+  difficulty: 'Easy',
+  statement: 'True or false.. I am amazing?',
+  multiChoiceOptions: [
+    'true', 'false'
+  ],
+  answer: {
+    match: 'true',
+    statement: '.. and also extremely modest.'
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionTF04 = {
+  uuid: 'questionTF04',
+  originatorInfo: originator1,
+  topic: 'Movies',
+  questionType: 'True or False',
+  difficulty: 'Easy',
+  statement: 'True or false.. Mrs. Robinson in The Graduate was played by Anne Bancroft?',
+  multiChoiceOptions: [
+    'true', 'false'
+  ],
+  answer: {
+    match: 'true',
+    statement: ''
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionTF05 = {
+  uuid: 'questionTF05',
+  originatorInfo: originator1,
+  topic: 'Movies',
+  questionType: 'True or False',
+  difficulty: 'Easy',
+  statement: "True or false.. The name of the skyscraper in Die Hard is \"The Nakatomi Towers\"",
+  multiChoiceOptions: [
+    'true', 'false'
+  ],
+  answer: {
+    match: 'false',
+    statement: "It was \"Nakatomi Plaza\""
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice01 = {
+  uuid: 'questionMultipleChoice01',
+  originatorInfo: originator1,
+  topic: 'Science',
+  questionType: 'Multiple Choice',
+  difficulty: 'Difficult',
+  statement: 'Which metal was discovered by Hans Christian Oersted?',
+  multiChoiceOptions: [
+    'Iron', 'Magnesium', 'Zinc', 'Aluminium'
+  ],
+  answer: {
+    match: 'Aluminium',
+    statement: 'It was discovered in 1825.'
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice02 = {
+  uuid: 'questionMultipleChoice02',
+  originatorInfo: originator2,
+  topic: 'Science',
+  questionType: 'Multiple Choice',
+  difficulty: 'Difficult',
+  statement: 'Which year was the first Tonka truck made?',
+  multiChoiceOptions: [
+    '1945', '1947', '1949'
+  ],
+  answer: {
+    match: '1947',
+    statement: ''
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice03 = {
+  uuid: 'questionMultipleChoice03',
+  originatorInfo: originator1,
+  topic: 'History',
+  questionType: 'Multiple Choice',
+  difficulty: 'Medium',
+  statement: 'Which year marked the beginning of the French revolution?',
+  multiChoiceOptions: [
+    '1689', '1789', '1889'
+  ],
+  answer: {
+    match: '1789',
+    statement: ''
+  }
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice04 = {
+  uuid: 'questionMultipleChoice04',
+  originatorInfo: originator1,
+  topic: 'history',
+  questionType: 'Multiple Choice',
+  difficulty: 'Easy',
+  statement: 'Who was British Prime Minister during the Falklands war?',
+  answer: {
+    match: 'Margaret Thatcher',
+    statement: ''
+  },
+  multiChoiceOptions: [
+    'Tony Blair', 'Margaret Thatcher', 'Gordon Brown'
+  ]
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice05 = {
+  uuid: 'questionMultipleChoice05',
+  originatorInfo: originator1,
+  topic: 'Movies',
+  questionType: 'Multiple Choice',
+  difficulty: 'Easy',
+  statement: 'Which 1982 film was greatly accepted for its portrayal of the love between a young,'
+    + ' fatherless suburban boy and a lost, benevolent and homesick visitor from another planet?',
+  answer: {
+    match: 'E. T. The Extra-Terrestrial',
+    statement: ''
+  },
+  multiChoiceOptions: [
+    'E. T. The Extra-Terrestrial', 'Rocky', 'The Wizard of Oz', 'Predator'
+  ]
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice06 = {
+  uuid: 'questionMultipleChoice06',
+  originatorInfo: originator1,
+  topic: 'Movies',
+  questionType: 'Multiple Choice',
+  difficulty: 'Easy',
+  statement: 'In The Matrix, does Neo take the blue pill or the red pill?',
+  answer: {
+    match: 'Red',
+    statement: "\"You take the blue pill – the story ends, you wake up in your bed and believe whatever you want to believe."
+      + " You take the red pill – you stay in Wonderland, and I show you how deep the rabbit hole goes.\""
+  },
+  multiChoiceOptions: [
+    'Red', 'Blue'
+  ]
+};
+
+/** Dev use only
+  * @ignore */
+const questionMultipleChoice07 = {
+  uuid: 'questionMultipleChoice07',
+  originatorInfo: originator1,
+  topic: 'Movies',
+  questionType: 'Multiple Choice',
+  difficulty: 'Easy',
+  statement: 'Who played Juror Number 8 in 12 Angry Men?',
+  answer: {
+    match: 'Henry Fonda',
+    statement: ''
+  },
+  multiChoiceOptions: [
+    'Dustin Hoffman', 'Henry Fonda', 'Robert Redford', 'David Soul'
+  ]
+};
+
+
+const dummyQuiz1 = {
+  uuid: '0001',
+  title: 'General Knowledge Quiz',
+  originatorInfo: originator1,
+  tags: ['general knowledge', 'sport', 'history'],
+  difficulty: 'Medium',
+  questions: [
+    questionTF01,
+    questionTF02,
+    questionTF03,
+    questionMultipleChoice01,
+    questionMultipleChoice02,
+    questionMultipleChoice03,
+    questionMultipleChoice04,
+    questionMultipleChoice05
+  ],
+  questionTypes: ['True or False', 'Multiple Choice']
+};
+
+
+/** Dev use only
+  * @ignore */
+const dummyQuiz2 = {
+  uuid: '0002',
+  title: 'Movie Time Quiz',
+  originatorInfo: originator2,
+  tags: ['Movies'],
+  difficulty: 'Medium',
+  questions: [
+    questionTF04,
+    questionTF05,
+    questionMultipleChoice05,
+    questionMultipleChoice06,
+    questionMultipleChoice07
+  ],
+  questionTypes: ['True or False', 'Multiple Choice']
+};
+
+/** Dev use only
+  * @ignore */
+const dummyQuiz3 = {
+  uuid: '0003',
+  title: 'Master Quiz',
+  originatorInfo: originator1,
+  tags: ['Movies', 'general knowledge', 'sport', 'history'],
+  difficulty: 'Medium',
+  questions: [
+    questionTF01,
+    questionTF02,
+    questionTF03,
+    questionTF04,
+    questionTF05,
+    questionMultipleChoice01,
+    questionMultipleChoice02,
+    questionMultipleChoice03,
+    questionMultipleChoice04,
+    questionMultipleChoice05,
+    questionMultipleChoice06,
+    questionMultipleChoice07
+  ],
+  questionTypes: ['True or False', 'Multiple Choice']
+};
+
+export const quizTestData = [
+  dummyQuiz1,
+  dummyQuiz2,
+  dummyQuiz3
+];
